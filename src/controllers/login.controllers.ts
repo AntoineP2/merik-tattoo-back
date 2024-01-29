9beeea4a8889a67257db16d92004098b3e26046e
AntoineP2 <antoine.vinsonneau34@gmail.com>
2024-01-29 13:15:32 +0100

    correction login

--- src/controllers/login.controllers.ts
+++ src/controllers/login.controllers.ts
@@ -7,27 +7,31 @@ import * as jwt from "jsonwebtoken";
 import { PRI_KEY_JWT, PUB_KEY_JWT } from "../config/dotenv";
 
 
-// JWT installé
 export const login = async (req: Request, res: Response) => {
-    const { userName, userPasswordCrypted } = req.body
-    const userInfo = await loginDao(userName)
-    if (!userInfo) {
-        return res.status(400).json("User not found")
-    }
-    const passwordDataBase = userInfo.password
-
-    bcrypt.compare(decryptPassword(userPasswordCrypted), passwordDataBase!, function (err: any, result: boolean) {
-        if (result == true) {
-        } else {
-            return res.status(400).json("wrong password")
+    try {
+        const { userName, userPasswordCrypted } = req.body
+        const userInfo = await loginDao(userName)
+        if (!userInfo) {
+            return res.status(400).json("User not found")
         }
-    })
-    const userInfoToSend = {
-        userName: userInfo.name,
-        userRole: userInfo.role
-    }
-    const token = jwt.sign({userInfo : userInfoToSend}, PRI_KEY_JWT!, { expiresIn: '24h', algorithm: 'RS256'})
+        const passwordDataBase = userInfo.password
 
-    // const decodeToken = jwt.verify(token, PUB_KEY_JWT!, { algorithms: ['RS256'] })
-    return res.status(200).json(token)
+        const isPasswordValid = await bcrypt.compare(decryptPassword(userPasswordCrypted), passwordDataBase!)
+        if (!isPasswordValid) {
+            return res.status(400).json("Wrong password")
+        }
+
+        const userInfoToSend = {
+            userName: userInfo.name,
+            userRole: userInfo.role
+        }
+        const token = jwt.sign({ userInfo: userInfoToSend }, PRI_KEY_JWT!, { expiresIn: '24h', algorithm: 'RS256' })
+
+        // const decodeToken = jwt.verify(token, PUB_KEY_JWT!, { algorithms: ['RS256'] })
+        return res.status(200).json(token)
+    }
+    catch (e) { 
+        console.error(e)
+    }
+
 }
